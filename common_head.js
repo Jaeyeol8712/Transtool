ace62985d54a499b80f0544096527dba8bf4915e
Jaeyeol8712 <wodufdl87@gmail.com>
2026-07-23 14:59:10 +0900

    Update common_head.js

--- common_head.js
+++ common_head.js
@@ -1,41 +1,33 @@
-// 모든 페이지에 공통으로 들어가는 애널리틱스 및 메타태그 관리
-(function() {
- const head = document.head;
+//모든페이지에공통으로적용되는태그관리
+(function(){
+	const head=document.head;
 
- // 1. 구글 서치콘솔 / 네이버 서치어드바이저 인증태그 (코드 입력 부분)
- const metaTags = [
- { name: 'google-site-verification', content: 'PomvSVjqf2y59zGMvY-9VAVV4vf07mF1uXXZ9L3cTHQ' },
- { name: 'naver-site-verification', content: '여기에_네이버_인증코드_입력' }
- ];
- 
- metaTags.forEach(tag => {
- const el = document.createElement('meta');
- el.name = tag.name;
- el.content = tag.content;
- head.appendChild(el);
- });
+	//1.메타태그추가(네이버서치어드바이저,구글애드센스)
+	const metaTags=[
+		{name:'naver-site-verification',content:'b49199b17146051daa9c8f8eba5053a31708444c'},
+		{name:'google-adsense-account',content:'ca-pub-7089972708901760'}
+	];
 
- // 2. 구글 애널리틱스 (GA4)
- const gaId = 'G-XXXXXXXXXX'; // GA4 측정 ID로 변경
- const gaScript = document.createElement('script');
- gaScript.async = true;
- gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
- head.appendChild(gaScript);
+	metaTags.forEach(tag=>{
+		const el=document.createElement('meta');
+		el.name=tag.name;
+		el.content=tag.content;
+		head.appendChild(el);
+	});
 
- const gaInit = document.createElement('script');
- gaInit.innerHTML = `
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
- gtag('config', '${gaId}');
- `;
- head.appendChild(gaInit);
+	//2.구글애널리틱스(GA4)적용
+	const gaId='G-2LK65NYW74';
+	const gaScript=document.createElement('script');
+	gaScript.async=true;
+	gaScript.src=`https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+	head.appendChild(gaScript);
 
- // 3. 구글 애드센스 자동광고 스크립트
- const adClientId = 'ca-pub-XXXXXXXXXXXXXXXX'; // 애드센스 펍ID로 변경
- const adScript = document.createElement('script');
- adScript.async = true;
- adScript.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adClientId}`;
- adScript.crossOrigin = 'anonymous';
- head.appendChild(adScript);
+	const gaInit=document.createElement('script');
+	gaInit.innerHTML=`
+		window.dataLayer=window.dataLayer||[];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js',new Date());
+		gtag('config','${gaId}');
+	`;
+	head.appendChild(gaInit);
 })();
