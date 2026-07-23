3bc70796b34662c1253cdfff626bafb6d0d7fdf6
Jaeyeol8712 <wodufdl87@gmail.com>
2026-07-23 15:04:37 +0900

    Update common_head.js

--- common_head.js
+++ common_head.js
@@ -1,33 +1,29 @@
-//모든페이지에공통으로적용되는태그관리
+// 모든 페이지에 공통으로 적용되는 태그 관리 (aroundtool.com)
 (function(){
-	const head=document.head;
-
-	//1.메타태그추가(네이버서치어드바이저,구글애드센스)
-	const metaTags=[
-		{name:'naver-site-verification',content:'b49199b17146051daa9c8f8eba5053a31708444c'},
-		{name:'google-adsense-account',content:'ca-pub-7089972708901760'}
-	];
-
-	metaTags.forEach(tag=>{
-		const el=document.createElement('meta');
-		el.name=tag.name;
-		el.content=tag.content;
-		head.appendChild(el);
-	});
-
-	//2.구글애널리틱스(GA4)적용
-	const gaId='G-2LK65NYW74';
-	const gaScript=document.createElement('script');
-	gaScript.async=true;
-	gaScript.src=`https://www.googletagmanager.com/gtag/js?id=${gaId}`;
-	head.appendChild(gaScript);
-
-	const gaInit=document.createElement('script');
-	gaInit.innerHTML=`
-		window.dataLayer=window.dataLayer||[];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js',new Date());
-		gtag('config','${gaId}');
-	`;
-	head.appendChild(gaInit);
+  const head = document.head;
+  // 1. 메타태그 추가 (네이버 서치어드바이저, 구글 애드센스)
+  const metaTags = [
+    { name: 'naver-site-verification', content: 'b49199b17146051daa9c8f8eba5053a31708444c' },
+    { name: 'google-adsense-account', content: 'ca-pub-7089972708901760' }
+  ];
+  metaTags.forEach(tag => {
+    const el = document.createElement('meta');
+    el.name = tag.name;
+    el.content = tag.content;
+    head.appendChild(el);
+  });
+  // 2. 구글 애널리틱스(GA4) 적용
+  const gaId = 'G-2LK65NYW74';
+  const gaScript = document.createElement('script');
+  gaScript.async = true;
+  gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  head.appendChild(gaScript);
+  const gaInit = document.createElement('script');
+  gaInit.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${gaId}');
+  `;
+  head.appendChild(gaInit);
 })();
